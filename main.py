95075af26c9e9f42da8f733a69f597f0fbc2ee5e
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-09-14 19:03:17 +0300

    final version for review

--- main.py
+++ main.py
@@ -27,10 +27,8 @@ def count_clicks(url, token):
         ('units', '-1'),
     )
     parsed_link = urlparse(url)
-    netloc = parsed_link.netloc
-    path = parsed_link.path
-    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{netloc}"
-                            f"{path}/clicks/summary",
+    response = requests.get(f"https://api-ssl.bitly.com/v4/bitlinks/{parsed_link.netloc}"
+                            f"{parsed_link.path}/clicks/summary",
                             headers=headers, params=params)
     response.raise_for_status()
     click_link = response.json()
@@ -54,20 +52,19 @@ def main():
     load_dotenv()
     user_token = os.environ["BITLY_TOKEN"]
     user_input = input('Введите ссылку: ')
-    is_bitlink(user_input, user_token)
-    if is_bitlink:
+    if is_bitlink(user_input, user_token):
         try:
             check_clicks = count_clicks(user_input, user_token)
             print(check_clicks)
         except requests.exceptions.HTTPError as error:
-            exit("Can't get data from server:\n{0}".format(error))
+            print("Can't get data from server:\n{0}".format(error))
 
-    elif is_bitlink is False:
+    else:
         try:
             bitlink = shorten_link(user_token, user_input)
             print(bitlink)
         except requests.exceptions.HTTPError as error:
-            exit("Can't get data from server:\n{0}".format(error))
+            print("Can't get data from server:\n{0}".format(error))
 
 
 if __name__ == '__main__':
